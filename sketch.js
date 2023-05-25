2a51c116cb0f673e15c1b1c794f1cb39cce6691a
cksdud419 <91289935+cksdud419@users.noreply.github.com>
2023-05-26 00:50:29 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-// 모션 추정 코드
 let video;
 let countSound;
 
@@ -14,9 +13,18 @@ let countName = 'None';
 let inp;
 let angle;
 
+// 화면 녹화 관련 변수
+let recordingBtn;
+let chunks = [];
+let mediaRecorder;
+let recording = false;
+
+// 모션 리스트 변수
+let motionList;
+
 function preload() {
   motionList = loadJSON('model/model_meta.json');
-} // 저장된 동작 리스트를 알기 위해 json파일을 프로그램 실행 전 미리 로드
+}
 
 function setup() {
   createCanvas(384, 512);
@@ -63,10 +71,6 @@ function toggleRecording() {
   }
 }
 
-let chunks = [];
-let mediaRecorder;
-let recording = false;
-
 function startRecording() {
   if (typeof MediaRecorder === 'undefined') {
     console.log('MediaRecorder API is not supported.');
@@ -103,62 +107,11 @@ function handleStop() {
   console.log('Recording stopped.');
 }
 
-
-// 각도는 양 옆 점(p1, p3) 사이에 끼인 점(p2)의 사이각을 구함
-// ex = getAngle(pose.leftShoulder, pose.leftElbow, pose.leftWrist)
-function getAngle(p1, p2, p3) {
-  if (
-    p1.confidence < 0.65 ||
-    p2.confidence < 0.65 ||
-    p3.confidence < 0.65
-  )
-    return null; // 키포인트 인식이 제대로 되지 않으면 각도 계산을 하지 않음
-
-  rad =
-    Math.atan2(p3.y - p2.y, p3.x - p2.x) -
-    Math.atan2(p1.y - p2.y, p1.x - p2.x); // atan2 함수를 이용하여 라디안 계산
-  deg = rad * (180 / Math.PI); // 라디안을 각도로 환산하기 위해 180/파이 곱
-  vecdeg = 360 - Math.abs(deg); // 반대쪽 각
-
-  if (deg < 0) return Math.abs(deg); // 음각일 때 절대값으로 반환
-  else return deg;
-}
-
-function angleUpdate() {
-  // 추가적인 각도 계산이 필요하면 추가할 것
-  leftElbow = getAngle(
-    pose.leftShoulder,
-    pose.leftElbow,
-    pose.leftWrist
-  ); //좌측 팔꿈치각도
-  rightElbow = getAngle(
-    pose.rightShoulder,
-    pose.rightElbow,
-    pose.rightWrist
-  ); //우측 팔꿈치각도
-
-  leftKnee = getAngle(pose.leftHip, pose.leftKnee, pose.leftAnkle); //좌측 무릎
-  rightKnee = getAngle(pose.rightHip, pose.rightKnee, pose.rightAnkle); //우측 무릎
-
-  leftShoulder = getAngle(
-    pose.leftElbow,
-    pose.leftShoulder,
-    pose.leftHip
-  ); //좌측 어깨(겨드랑이)
-  rightShoulder = getAngle(
-    pose.rightElbow,
-    pose.rightShoulder,
-    pose.rightHip
-  ); //우측 어깨
-
-  return {
-    leftElbow: leftElbow,
-    rightElbow: rightElbow,
-    leftKnee: leftKnee,
-    rightKnee: rightKnee,
-    leftShoulder: leftShoulder,
-    rightShoulder: rightShoulder
-  };
+function extraction(poses) {
+  if (poses.length > 0) {
+    pose = poses[0].pose;
+    skeleton = poses[0].skeleton;
+  }
 }
 
 function classification() {
@@ -170,7 +123,7 @@ function classification() {
     }
     brain.classify(inputs, classifyResult);
   } else {
-    setTimeout(classification, 100); //포즈가 인식되지 않았을 때 100밀리초마다 포즈추정 반복
+    setTimeout(classification, 100);
   }
 }
 
@@ -187,18 +140,64 @@ function classifyResult(error, results) {
     countSound.play();
   }
 
-  classification(); //반복 포즈추정
+  classification();
 }
 
-function extraction(poses) {
-  if (poses.length > 0) {
-    pose = poses[0].pose;
-    skeleton = poses[0].skeleton;
-  }
+function getAngle(p1, p2, p3) {
+  if (
+    p1.confidence < 0.65 ||
+    p2.confidence < 0.65 ||
+    p3.confidence < 0.65
+  )
+    return null;
+
+  rad =
+    Math.atan2(p3.y - p2.y, p3.x - p2.x) -
+    Math.atan2(p1.y - p2.y, p1.x - p2.x);
+  deg = rad * (180 / Math.PI);
+  vecdeg = 360 - Math.abs(deg);
+
+  if (deg < 0) return Math.abs(deg);
+  else return deg;
+}
+
+function angleUpdate() {
+  leftElbow = getAngle(
+    pose.leftShoulder,
+    pose.leftElbow,
+    pose.leftWrist
+  );
+  rightElbow = getAngle(
+    pose.rightShoulder,
+    pose.rightElbow,
+    pose.rightWrist
+  );
+
+  leftKnee = getAngle(pose.leftHip, pose.leftKnee, pose.leftAnkle);
+  rightKnee = getAngle(pose.rightHip, pose.rightKnee, pose.rightAnkle);
+
+  leftShoulder = getAngle(
+    pose.leftElbow,
+    pose.leftShoulder,
+    pose.leftHip
+  );
+  rightShoulder = getAngle(
+    pose.rightElbow,
+    pose.rightShoulder,
+    pose.rightHip
+  );
+
+  return {
+    leftElbow: leftElbow,
+    rightElbow: rightElbow,
+    leftKnee: leftKnee,
+    rightKnee: rightKnee,
+    leftShoulder: leftShoulder,
+    rightShoulder: rightShoulder
+  };
 }
 
 function draw() {
-  //push();
   translate(video.width, 0);
   scale(-1, 1);
   image(video, 0, 0, width, height);
@@ -219,5 +218,4 @@ function draw() {
       line(a.position.x, a.position.y, b.position.x, b.position.y);
     }
   }
-  //pop();
 }
